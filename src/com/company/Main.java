164ee9763e4cfcd7546d50c6a7e855b04b0a10f6
Faraday <faradaytrs@gmail.com>
2014-10-15 21:54:57 +0400

    ADDED STRING

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static Matrix matrix = new Matrix();
     public static Number number = new Number();
 
+	//THIS STRING ADDED
+
     public static void main(String[] args) {
         matrix.printMatrix(matrix.createMatrix(matrix.readMatrixSize()));
         System.out.println(number.isArmstrong(Number.readOneNumber()));
